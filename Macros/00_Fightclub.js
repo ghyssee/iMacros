0b4ed7cc62f2fc07de437e2b16feacaffe29ad73
ghyssee <eric.ghyssens@hotmail.nl>
2022-07-12 23:39:26 +0200

    fightclub changes

--- Macros/00_Fightclub.js
+++ Macros/00_Fightclub.js
@@ -17,16 +17,28 @@ var MACRO_FOLDER = "MR";
 var MR_ID = "-1";
 
 var FILENAME = new ConfigFile(getPath(PATH_PROCESS), ALBUM + ".json");
-//var albumArtist = selectArtist(); // 171
 
-//processAlbum();
+
+
 var counter = 0;
-setMyId();
+joinFightClub();
+do {
+	setMyId();
+	waitV2("0.5");
+}
+while (MR_ID == "-1");
+
 do {
 	var myCounter = startFight();
 	counter++;
 }
-while (counter < 100 && myCounter != -1);
+while (counter < 100 && myCounter != -1 && counter != -1);
+
+
+function joinFightClub(){
+	logV2(INFO, CATEGORY, "Join Fight Club");
+	var retCode = simpleMacroPlayFolder("11_Fightclub_Join.iim", MACRO_FOLDER);
+}
 
 function setMyId(){
 	logV2(INFO, CATEGORY, "setMyId");
@@ -46,32 +58,85 @@ function setMyId(){
 	}
 }
 
+function checkResultsButton(){
+	var oSpan = window.content.document.querySelectorAll("a[data-params*=result]");
+	//logV2(INFO, CATEGORY, "checkResultsButton: " + oSpan.length);
+	if (oSpan.length > 1){
+		return 1;
+	}
+	return 0;
+}
+
 function getMyHealth(){
 	logV2(INFO, CATEGORY, "getMyHealth");
 	var oSpan = null;
-	var health = 100;
-	// contain id fc-health but not class health_bar
-	oSpan = window.content.document.querySelectorAll("span[id=fc-health-" + MR_ID);
-	for (var i=0; i < oSpan.length; i++){
-		//logV2(INFO, CATEGORY, oSpan[i].innerText);
-		//logV2(INFO, CATEGORY, oSpan[i].outerHTML);
-		var health = oSpan[i].innerText;
-		health = health.replace(/(.*)\%/g, "$1");
-		var attr= oSpan[i].getAttribute('id');
-		attr = attr.replace(/fc-health-(.*)/g, "$1");
-		//logV2(INFO, CATEGORY, "health: " + health);
-		return health;
+	var health = 0;
+	do {
+		// checking for results button
+		var results = checkResultsButton();
+		//logV2(INFO, CATEGORY,"checkResultsButton: " + results);
+		if (results == 1){
+			// results button available. No need for checking health anymore
+			return 0;
+		}
+		// gettting my health
+		oSpan = window.content.document.querySelectorAll("span[id=fc-health-" + MR_ID + "]");
+		logV2(INFO, CATEGORY,"getMyHealth oSpan.length: " + oSpan.length);
+
+		for (var i=0; i < oSpan.length; i++){
+			//logV2(INFO, CATEGORY, oSpan[i].innerText);
+			//logV2(INFO, CATEGORY, oSpan[i].outerHTML);
+			var health = oSpan[i].innerText;
+			health = health.replace(/(.*)\%/g, "$1");
+			var attr= oSpan[i].getAttribute('id');
+			attr = attr.replace(/fc-health-(.*)/g, "$1");
+			logV2(INFO, CATEGORY, "My health: " + health);
+			return health;
+		}
+		if (oSpan.length == 0){	
+			logV2(INFO, CATEGORY, "Reload page");
+			waitV2(0.5);
+			
+		}
 	}
+	while (oSpan.length == 0);
 	return health;
+}
+
+function getFighters(){
+	// contain id fc-health but not class health_bar
+	var nr = 0;
+	//var tries = 0;
+	var oSpan = null;
+	do {
+		
+		var results = checkResultsButton();
+		logV2(INFO, CATEGORY,"checkResultsButton: " + results);
+		if (results == 1){
+			// results button available. No need for checking health anymore
+			return null;
+		}
+		oSpan = window.content.document.querySelectorAll("span[id*=fc-health]:not([class*=health_bar])");
+		nr = oSpan.length;
+		if (nr != 5){
+			waitV2(0.1);
+		}
+		//tries++;
+		//logV2(INFO, CATEGORY, "nr: " + nr);
+		//logV2(INFO, CATEGORY, "tries: " + tries);
+	}
+	while (nr != 5 && results == 0); //&& tries < 100);
+	return oSpan;
 }
 
 	
 function startFight(){
-	var oSpan = null;
+	var oSpan = getFighters();
+	if (oSpan == null){
+		alert("No Fighters available. Fight Finished!");
+	}
 	var counter = 0;
 	
-	// contain id fc-health but not class health_bar
-	oSpan = window.content.document.querySelectorAll("span[id*=fc-health]:not([class*=health_bar])");
 	var nr = oSpan.length;
 	var fighters = new Array();
 	//for (var i=0; i < oSpan.length; i++){
@@ -83,7 +148,7 @@ function startFight(){
 		}
 		var fighterObj = {"id": null, "health": -1};
 		//logV2(INFO, CATEGORY, oSpan[i].innerText);
-		//logV2(INFO, CATEGORY, oSpan[i].outerHTML);
+		logV2(INFO, CATEGORY, oSpan[i].outerHTML);
 		var health = oSpan[i].innerText;
 		health = health.replace(/(.*)\%/g, "$1");
 		var attr= oSpan[i].getAttribute('id');
@@ -95,7 +160,7 @@ function startFight(){
 		//logV2(INFO, CATEGORY, "health: " + health);
 		//logV2(INFO, CATEGORY, "attr: " + attr);
 		if (Number(health) > 0){
-			logV2(INFO, CATEGORY, "Is Not Dead yet");
+			//logV2(INFO, CATEGORY, "Is Not Dead yet");
 			fighterObj.id = attr;
 			fighterObj.health  = Number(health);
 			counter++;
@@ -104,15 +169,19 @@ function startFight(){
 			//break;
 		}
 	}
-	//logV2(INFO, CATEGORY, JSON.stringify(fighters));
+	logV2(INFO, CATEGORY, JSON.stringify(fighters));
 	var attackObj = getFighterLowestHealth(fighters);
-	logV2(INFO, CATEGORY, JSON.stringify(attackObj));
+	logV2(INFO, CATEGORY, "Lowest Health Player: " + JSON.stringify(attackObj));
 	if (attackObj != null && attackObj != null){
-		attack(attackObj.id);
+		var retCode = attack(attackObj.id);
+		//if (retCode != 1){
+	//		return -1;
+	//	}
 		//simulateClick(attackObj.id);
 	}
 	else {
 		logV2(INFO, CATEGORY, "There was a problem with attacking lowest health player");
+		return counter;
 	}
 	return counter;
 }
@@ -148,6 +217,10 @@ function attack(id){
 	logV2(INFO, CATEGORY, "Attacking " + id);
 	iimSet("id", id);
 	var retCode = simpleMacroPlayFolder("10_Attack_Fightclub.iim", MACRO_FOLDER);
+	if (retCode == -921){
+		logV2(INFO, CATEGORY, "Player is probably already dead");
+	}
+	return retCode;
 }
 
 function readScript(filename){
